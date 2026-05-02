4d89bd9080c53fd3a63f37f1d747ab65e4cd3742
ksathvikreddy <ksathvikreddy31@gmail.com>
2026-05-02 08:16:02 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+import models, schemas
+from database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Allow React frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home():
+    return {"message": "TaskFlowCloud Backend Running"}
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
+
+@app.post("/tasks")
+def add_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    new_task = models.Task(title=task.title)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+@app.put("/tasks/{task_id}")
+def complete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    task.status = "Completed"
+    db.commit()
+    return task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db.delete(task)
+    db.commit()
+
+    return {"message": "Deleted Successfully"}
---
+++ backend/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    status: str
+
+    class Config:
+        from_attributes = True
